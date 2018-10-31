594d4ecdf2410dc152c412943016c33e8964a0dc
John Wood <johnnathanwood@gmail.com>
2018-10-31 13:04:07 -0500

    practice in class work

---
+++ src/js/main.js
@@ -0,0 +1,57 @@
+class BigScaryBird {
+
+    constructor () {
+        this.name = "Big Bird"
+        this.height = "7ft"
+        this.flightless = true
+        this.isExtinct= true
+        this.runningSpeed = "10:00 pm"
+    }
+}
+console.table(BigScaryBird)
+  // Properties of Joey's House of Pizza
+const domesticWildTurkey = new bird({
+    name: "Meleagris Gallopavo",
+    height: "4ft",
+    flightless: true,
+    isExtinct: true,
+    runningSpeed: "2:00 pm"
+})
+const Ostrich = new bird({
+    name: "Struthio Camelus",
+    height: "7ft",
+    flightless: true,
+    isExtinct: false,
+    runningSpeed: "2:00 pm"
+})
+const Emu = new bird({
+    name: "Dromaius Novaehollandiae",
+    height: "5ft",
+    flightless: true,
+    isExtinct: false,
+    runningSpeed: "2:00 pm"
+})
+const emperorPenguin = new bird({
+    name: "Aptenodytes Forsteri",
+    height: "3.7ft",
+    flightless: true,
+    isExtinct: false,
+    runningSpeed: "2:00 pm"
+})
+
+
+
+// toString() {
+//     return `The ${this.name} is a big, scary bird that stands up to ${this.height} and can run up to ${this.runningSpeed}!`
+// }
+
+// const cassowaryProps = new bird({
+//     height: "6 ft",
+//     isExtinct: false,
+//     name: "cassowary",
+//     runningSpeed: "30 mph"
+// })
+// const cassowary  = new BigScaryBird(cassowaryProps)
+// console.log(cassowary.toString())
+
+
